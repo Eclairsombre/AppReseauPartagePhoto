19e7a9dca872cf1d389936b2279239e20fad07a3
Eclairsombre <alexmt14095@gmail.com>
2025-11-28 16:41:54 +0100

    feat: Add UserController

---
+++ src/main/java/local/epul4a/fotoshare/controller/UserController.java
@@ -0,0 +1,31 @@
+package local.epul4a.fotoshare.controller;
+
+import local.epul4a.springbootdatajpa.model.User;
+import local.epul4a.springbootdatajpa.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @GetMapping("/users")
+    public String getAllUsers(Model model) {
+        model.addAttribute("users", userService.getAllUsers());
+        return "users";
+    }
+    @GetMapping("/addUser")
+    public String showUserForm(Model model) {
+        model.addAttribute("user", new User());
+        return "addUser";
+    }
+    @PostMapping("/addUser")
+    public String addUser(@ModelAttribute User user) {
+        userService.saveUser(user);
+        return "redirect:/users";
+    }
+}
